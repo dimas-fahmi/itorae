a46c051fdd9c51f70efea4eb3b9939e34d2b7da6
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-07-02 19:40:59 +0700

    Remove unused global styles and update font variables in globals.tailwind.css; add landing page component in page.tsx

---
+++ app/[public]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Japanese & Italian Cuisine | ITORAE",
+};
+
+const LandingPage = () => {
+  return <div>ITORAE</div>;
+};
+
+export default LandingPage;
