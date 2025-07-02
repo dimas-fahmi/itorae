536e133153903a5f7b54b3282c2a5658e34ee6f4
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-07-02 19:12:16 +0700

    project: setup

---
+++ src/ui/fonts/index.ts
@@ -0,0 +1,28 @@
+import { Poppins, Shadows_Into_Light, Oswald } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+  display: "swap",
+  preload: true,
+  fallback: ["sans-serif"],
+});
+
+export const shadowsIntoLight = Shadows_Into_Light({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-shadows-into-light",
+  display: "swap",
+  preload: true,
+  fallback: ["cursive"],
+});
+
+export const oswald = Oswald({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-oswald",
+  display: "swap",
+  preload: true,
+  fallback: ["sans-serif"],
+});
